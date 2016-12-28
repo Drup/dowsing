d52a1c4f89a9662a59d475dd6cb2c8163b5d64f8
Drup <drupyog@zoho.com>
2016-12-28 01:09:35 +0100

    Complete normalisation.

--- src/normal_form.ml
+++ src/normal_form.ml
@@ -11,8 +11,8 @@ end
 
 module rec Ty : sig
   type t =
-    | Var of string
-    | Constr of P.t * t list
+    | Var of int
+    | Constr of P.t * t array
     | Arrow of NSet.t * t
     | Tuple of NSet.t
     | Unknown of Outcometree.out_type
@@ -28,27 +28,29 @@ and Op : Set.OrderedType with type t = Ty.t = struct
     | Unknown _ -> 4
 
   let rec compare lhs rhs = let open Ty in match (lhs, rhs) with
-    | Var lhs0, Var rhs0 -> String.compare lhs0 rhs0
+    | Var lhs0, Var rhs0 -> CCInt.compare lhs0 rhs0
     | Constr (c1,arg1), Constr (c2,arg2) ->
       (match P.compare c1 c2 with
-       | 0 -> CCList.compare compare arg1 arg2
+       | 0 -> CCArray.compare compare arg1 arg2
        | x -> x)
     | Arrow (arg1,ret1), Arrow (arg2,ret2) ->
       (match compare ret1 ret2 with
        | 0 -> NSet.compare arg1 arg2
        | x -> x)
-    (* | Tuple lhs0, Tuple rhs0 -> NSet.compare lhs0 rhs0 *)
+    | Tuple lhs0, Tuple rhs0 -> NSet.compare lhs0 rhs0
     | Unknown lhs0, Unknown rhs0 ->
       Pervasives.compare lhs0 rhs0
     | _ ->
-      Pervasives.compare (to_int lhs) (to_int rhs)
+      CCInt.compare (to_int lhs) (to_int rhs)
 
 end
 and NSet
-  : Set.S with type elt = Ty.t
-  = Set.Make(Op)
+  : Custom_set.S with type elt = Ty.t
+  = Custom_set.Array(Op)
 
 include Op
+
+let equal x y = compare x y = 0
 
 module Cstr = struct
   open Ty
@@ -62,22 +64,39 @@ module Cstr = struct
     | arg, ret -> Arrow (NSet.singleton arg, ret)
 
   let tuple f tupl =
-    let aux set x = match f x with
-      | Tuple tup -> NSet.union set tup
-      | res -> NSet.add res set
-    in
-    Tuple (List.fold_left aux NSet.empty tupl)
+    Tuple
+      (NSet.of_seq @@ Sequence.map f @@ Sequence.of_list tupl)
 
 end
 
-let rec of_outcometree x =
+module STbl = CCHashtbl.Make(struct
+    include String
+    let hash = Hashtbl.hash
+  end)
+
+let symbol tbl s =
+  match STbl.get tbl s with
+  | Some i -> i
+  | None ->
+    let i = STbl.length tbl + 1 in
+    STbl.add tbl s i ;
+    i
+
+
+let rec of_outcometree_rec tbl x =
   let open Outcometree in match x with
   | Otyp_arrow (_label, arg, ret) ->
-    Cstr.arrow (of_outcometree arg) (of_outcometree ret)
+    Cstr.arrow (of_outcometree_rec tbl arg) (of_outcometree_rec tbl ret)
   | Otyp_constr (id, args) ->
-    Ty.Constr (P.of_outcometree id, List.map of_outcometree args)
-  | Otyp_tuple typ -> Cstr.tuple of_outcometree typ
-  | Otyp_var (_, s) -> Ty.Var s
+    let a = Sequence.(
+        to_array @@
+        map (of_outcometree_rec tbl) @@
+        of_list args)
+    in
+    Ty.Constr (P.of_outcometree id, a)
+  | Otyp_tuple typ -> Cstr.tuple (of_outcometree_rec tbl) typ
+  | Otyp_var (_, s) ->
+    Ty.Var (symbol tbl s)
 
   (* Not handled *)
   | Otyp_object (_,_)
@@ -96,6 +115,35 @@ let rec of_outcometree x =
   | Otyp_record _
   | Otyp_sum _
     -> Unknown x
+
+let renumber x nf =
+  let init_el = -1 in
+  let l = ref 0 in
+  let a = Array.make x init_el in
+  let resym i =
+    let e = a.(i) in
+    if e = init_el then begin
+      let new_e = !l in
+      a.(i) <- new_e ;
+      incr l ;
+      new_e
+    end else
+      e
+  in
+  let rec aux x = let open Ty in match x with
+    | Var i -> Var (resym i)
+    | Constr (p,args) -> Constr (p, Array.map aux args)
+    | Arrow (args,ret) -> Arrow (NSet.map aux args, ret)
+    | Tuple tup -> Tuple (NSet.map aux tup)
+    | Unknown _ -> x
+  in
+  aux nf
+
+let of_outcometree x =
+  let tbl = STbl.create 17 in
+  let nf = of_outcometree_rec tbl x in
+  renumber (STbl.length tbl) nf
+
 
 (*
  * Copyright (c) 2016 Gabriel Radanne <drupyog@zoho.com>
--- src/normal_form.mli
+++ src/normal_form.mli
@@ -7,14 +7,16 @@ end
 
 module rec Ty : sig
   type t =
-    | Var of string
-    | Constr of P.t * t list
+    | Var of int
+    | Constr of P.t * t array
     | Arrow of NSet.t * t
     | Tuple of NSet.t
     | Unknown of Outcometree.out_type
 end
-
-and NSet : Set.S with type elt = Ty.t
+and NSet : Custom_set.S with type elt = Ty.t
 
 type t = Ty.t
 val compare : t -> t -> int
+val equal : t -> t -> bool
+
+val of_outcometree : Outcometree.out_type -> t
