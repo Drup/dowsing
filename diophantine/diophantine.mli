913e24c5dc21e1b809f69443f2133e83d67c93a5
Drup <drupyog@zoho.com>
2017-02-04 19:58:56 +0100

    Small doc improvements.

--- diophantine/diophantine.mli
+++ diophantine/diophantine.mli
@@ -21,16 +21,19 @@
 
 (**
    Resolution of systems of linear Diophantine equations
-   @see https://www.lri.fr/~contejea/publis/biblio.html#contejean94ic
+
+   Extracted from cime v3.0
+   @author The CIME Team
+   @see <https://www.lri.fr/~contejea/publis/biblio.html#contejean94ic> An efficient algorithm for solving systems of diophantine equations by Evelyne Contejean and Hervé Devie.
 *)
 
 (**
   [solve v_type system] returns the minimal solutions [m]s of the linear
-   Diophantine system [system] such that
-   ∀i, [i > v_type.(0)] implies [m.(i) <= 1].
-   if there is a pair [(i,k)] such that
+   Diophantine system [system] such that:
+   - [∀i], [i > v_type.(0)] implies [m.(i) <= 1].
+   - if there is a pair [(i,k)] such that
    [v_type.(k) <= i < v_type.(k+1)] and [m.(i) > 0], then
-   ∀ [k' <> k], ∀ [i'],  [v_type.(k') <= i' < v_type.(k'+1)]
+   [∀ k' <> k], [∀i'],  [v_type.(k') <= i' < v_type.(k'+1)]
    implies [m.(i') = 0].
 *)
 val solve :
