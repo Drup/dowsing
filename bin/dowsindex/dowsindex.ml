411c825cba960427bbf78773f27f04c20d5f58ed
Clement Allain <clement.allain@enpc.fr>
2021-04-01 12:54:13 +0200

    handle syntax errors in dowsindex

--- bin/dowsindex/dowsindex.ml
+++ bin/dowsindex/dowsindex.ml
@@ -4,10 +4,18 @@ let usage () =
   CCFormat.printf "usage: dowsindex [unify | save | test] <args>...@." ;
   exit 1
 
+let error msg =
+  CCFormat.printf "error: %s@." msg ;
+  exit 1
+
+let type_of_string env str =
+  try Type.of_string env str
+  with Syntaxerr.Error _ -> error "syntax error"
+
 let unify str1 str2 =
   let env = Type.Env.make () in
-  let ty1 = Type.of_string env str1 in
-  let ty2 = Type.of_string env str2 in
+  let ty1 = type_of_string env str1 in
+  let ty2 = type_of_string env str2 in
   CCFormat.printf "@[<2>t1:@ %a@]@." (Type.pp env.var_names) ty1 ;
   CCFormat.printf "@[<2>t2:@ %a@]@." (Type.pp env.var_names) ty2 ;
   let unifs = Iter.to_list @@ Unification.unify env [ ty1, ty2 ] in
@@ -19,7 +27,7 @@ let save file_name =
 
 let test file_name str =
   let env = Type.Env.make () in
-  let ty' = Type.of_string env str in
+  let ty' = type_of_string env str in
   let idx = Index.load file_name in
   Timer.start timer ;
   idx
@@ -30,19 +38,19 @@ let test file_name str =
 
 let () =
   CCFormat.set_margin 100 ;
-  if Array.length Sys.argv < 2 then
+  if CCArray.length Sys.argv < 2 then
     usage () ;
   match Sys.argv.(1) with
   | "unify" ->
-      if Array.length Sys.argv < 4 then
+      if CCArray.length Sys.argv < 4 then
         usage () ;
       unify Sys.argv.(2) Sys.argv.(3)
   | "save" ->
-      if Array.length Sys.argv < 3 then
+      if CCArray.length Sys.argv < 3 then
         usage () ;
       save Sys.argv.(2)
   | "test" ->
-      if Array.length Sys.argv < 4 then
+      if CCArray.length Sys.argv < 4 then
         usage () ;
       test Sys.argv.(2) Sys.argv.(3)
   | _ ->
