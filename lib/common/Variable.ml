5a1cff11fe6f7900483e9188cf72b99c59a0a0a8
Clement Allain <clement.allain@enpc.fr>
2021-05-05 01:01:19 +0200

    aesthetics

--- lib/common/Variable.ml
+++ lib/common/Variable.ml
@@ -3,13 +3,16 @@ type t = Int.t
 module Gen = struct
 
   type var = t
-  type t = {
-    mutable content : int ;
-    dir : int ;
-  }   
 
-  let dir_as_int = function `Data -> 1 | `Query -> -1
-  
+  type t = {
+    mutable content : Int.t ;
+    dir : Int.t ;
+  }
+
+  let dir_as_int = function
+    | `Data -> 1
+    | `Query -> -1
+
   let make dir = {
     content = 0 ;
     dir = dir_as_int dir ;
@@ -29,16 +32,18 @@ module Map = CCMap.Make (CCInt)
 module HMap = CCHashtbl.Make (CCInt)
 module Set = CCSet.Make (CCInt)
 
-let rec base_26 start_chr i =
-  assert (i >= 0);
-  Char.chr (i mod 26 + start_chr) :: (
-    if i < 26 then []
-    else
-      base_26 start_chr (i/26)
-  )
-let to_string i =
-  let root = if i >= 0 then Char.code 'a' else Char.code 'A' in
-  CCString.of_list @@ base_26 root @@ abs i
+let to_string =
+  let rec base_26 start_chr i =
+    assert (i >= 0) ;
+    Char.chr (i mod 26 + start_chr) :: (
+      if i < 26 then []
+      else
+        base_26 start_chr (i / 26)
+    )
+  in
+  fun i ->
+    let root = Char.code @@ if i >= 0 then 'a' else 'A' in
+    CCString.of_list @@ base_26 root @@ abs i
 
 let pp fmt t =
-  Fmt.pf fmt "'%s" (to_string t)
+  Fmt.pf fmt "'%s" @@ to_string t
