0ec3df60e3e0ed65ed535b299ea5ea8db5ce7005
Drup <drupyog@zoho.com>
2022-05-25 18:43:32 +0200

    Use Atomics just in case with do something parallel one day.

--- lib/common/Timeout.ml
+++ lib/common/Timeout.ml
@@ -4,29 +4,34 @@ type state =
   | No_timeout
   | Timeout_until of float
 
-let state = ref No_timeout
+let state = Atomic.make No_timeout
 
 exception Timeout
 
 let check () =
-  match !state with
+  match Atomic.get state with
   | No_timeout -> ()
   | Timeout_until f ->
       if Unix.gettimeofday () > f then raise Timeout else ()
 
 let with_timeout time f =
-  begin match !state with No_timeout -> () | Timeout_until _ ->
+  let started_timeout =
+    Atomic.compare_and_set
+      state
+      No_timeout
+      (Timeout_until (Unix.gettimeofday () +. time))
+  in
+  if not started_timeout then
     invalid_arg "with_timeout: Nested timeouts are not allowed"
-  end;
-  state := Timeout_until (Unix.gettimeofday () +. time);
-  match f () with
-  | v ->
-    state := No_timeout;
-    Ok v
-  | exception Timeout ->
-    Logs.debug (fun m -> m "Timeout"); 
-    state := No_timeout;
-    Error ()
-  | exception exn ->
-    state := No_timeout;
-    raise exn
+  else
+    match f () with
+    | v ->
+      Atomic.set state No_timeout;
+      Ok v
+    | exception Timeout ->
+      Logs.debug (fun m -> m "Timeout"); 
+      Atomic.set state No_timeout;
+      Error ()
+    | exception exn ->
+      Atomic.set state No_timeout;
+      raise exn
