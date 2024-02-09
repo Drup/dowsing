815ccec07a711248c23e7c11dad7bdf9f583964a
Fardale <fardale+git@crans.org>
2024-02-09 10:36:01 +0100

    Send debug message to the trace also

--- lib/unification/Unification.ml
+++ lib/unification/Unification.ml
@@ -9,7 +9,11 @@ module Trace = Trace_core
 module Logs = (val Logs.(src_log @@ Src.create __MODULE__))
 
 let _info = Logs.info
-let debug = Logs.debug
+
+let debug f =
+  let s = f CCFormat.sprintf in
+  Logs.debug (fun m -> m "%s" s);
+  Trace.message ~data:(fun () -> [ ("message", `String s) ]) "Debug"
 
 (** {2 A stack of unification pairs} *)
 module Stack : sig
