5162bb90c54b91e9ceb691027722a543013767aa
Gabriel Radanne <gabriel.radanne@inria.fr>
2024-03-15 16:39:49 +0100

    Remove the libindex-related code.

--- lib/import/Dowsing_libindex.mli
+++
@@ -1,5 +0,0 @@
-(** Import using LibIndex *)
-
-val iter : string -> Fpath.t -> Db.Entry.t Iter.t
-(** [iter pkg dir] iterates over all entries available in
-    the various [*.cm*] files available in [dir]. *)
