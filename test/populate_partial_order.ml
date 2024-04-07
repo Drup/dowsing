ed6df6c6abb58f85fc3b938dc1f0016708772995
Gabriel Radanne <gabriel.radanne@inria.fr>
2024-04-07 23:31:35 +0200

    Tweak the tests.

--- test/populate_partial_order.ml
+++ test/populate_partial_order.ml
@@ -7,14 +7,6 @@ let () = Logs.set_reporter (Logs.format_reporter ())
 let env = Common.Type.Env.make ()
 let x = P.init env
 
-let add t =
-  (* Format.printf "%a@." Common.TypeId.pp t; *)
-  P.add x t
-
-let _from_index () =
-  let idx = Db.load @@ Fpath.v Sys.argv.(1) in
-  Idx.T.iterid idx.idx.trie add
-
 let _from_list string_types =
   let types =
     CCList.mapi (fun i s -> TypeId.mk i @@ Type.of_string env s) string_types
@@ -29,12 +21,6 @@ let _from_list string_types =
   in
   aux types
 
-(* let () =
-   _from_index ();
-   Format.printf "Size: %i@." (P.size x);
-   Format.printf "Number of edges : %i@." (P.connectivity x);
-   P.xdot x *)
-
 let () =
   let () =
     _from_list
@@ -45,7 +31,7 @@ let () =
         "int -> 'a";
         "'a -> 'b";
         "'a * 'c -> 'b";
-        (* "int -> float list -> 'a" *)
+        "int -> float list -> 'a";
         "float list -> int -> float list";
         "float -> int";
         "float -> float list";
