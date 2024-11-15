2c27cdb9acefc2e1b496a0d58dfd26a914ef0d73
Fardale <fardale+git@crans.org>
2024-11-15 15:55:55 +0100

    chore: format and correct binary name of stat

--- stat/main.ml
+++ stat/main.ml
@@ -5,7 +5,7 @@ let cmds = [ Bench.stat; Compare.compare ]
 let main_cmd, main_info =
   let doc = "Benchmark utils for Dowsing" in
   ( Term.(ret (const (`Error (true, "no command")))),
-    Cmd.info "bench" ~sdocs:Manpage.s_common_options ~doc )
+    Cmd.info "stat/main.exe" ~sdocs:Manpage.s_common_options ~doc )
 
 let () =
   Logs.(set_reporter @@ format_reporter ());
